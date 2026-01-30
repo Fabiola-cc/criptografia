16d6d9b711a904217f4f7148b93b2996f58ae2c3
Fabiola Contreras <fcontrerascol@gmail.com>
2026-01-29 19:16:32 -0600

    feat: generate dynamic keys

---
+++ keys/dynamic_keys.py
@@ -0,0 +1,19 @@
+import random
+import sys
+from pathlib import Path
+
+# Agregar el directorio padre al path
+sys.path.append(str(Path(__file__).parent.parent))
+
+from conversions.ascii_dict import dec_ascii_256
+
+def ascii_key(length):
+    key = ''
+    for i in range(length):
+        num = random.randint(33, 126) # numero aleatorio de caracteres imprimibles
+        key += dec_ascii_256.get(num) # valor en ascii
+
+    return key
+
+print("Ejemplo: ")
+print("Llave longitud 3 generada: " + ascii_key(3))
