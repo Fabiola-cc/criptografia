3f7706416a20f0d348e48712415b622b45fcfd5b
Fabiola Contreras <fcontrerascol@gmail.com>
2026-02-02 21:05:09 -0600

    feat: implement ceasar cypher

---
+++ historical/ceasar_cypher.py
@@ -0,0 +1,31 @@
+alfabeto_idx = {chr(i): i - ord('a') for i in range(ord('a'), ord('z') + 1)}
+alfabeto = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+
+def cesar_cifrar(mensaje, desplazamiento):
+    mensaje_cifrado = ""
+    for char in mensaje:
+        if char in alfabeto_idx:
+            new_position = (alfabeto_idx[char] + desplazamiento) % 26
+            mensaje_cifrado += alfabeto[new_position]
+        else:
+            mensaje_cifrado += char
+        
+    return mensaje_cifrado
+
+def cesar_descifrar(mensaje, desplazamiento):
+    mensaje_descifrado = ""
+    for char in mensaje:
+        if char in alfabeto_idx:
+            new_position = (alfabeto_idx[char] - desplazamiento) % 26
+            mensaje_descifrado += alfabeto[new_position]
+        else:
+            mensaje_descifrado += char
+    
+    return mensaje_descifrado
+
+print("Ejemplo de ceasar cypher")
+msg = "hola mundo"
+cifrado = cesar_cifrar(msg, 4)
+print(" Cifrar 'hola mundo' con desplazamiento 4: " + cifrado)
+print(" Proceso inverso: " + cesar_descifrar(cifrado, 4))  # hola mundo
+
